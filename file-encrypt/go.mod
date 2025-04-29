8c01f4a7ae94e04455354a58958a78d9a4823857
moyaqoob <moyaqoob28@gmail.com>
2025-04-30 02:50:31 +0530

    add

---
+++ file-encrypt/go.mod
@@ -0,0 +1,8 @@
+module file-encrypt
+
+go 1.23.2
+
+require (
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
+)
---
+++ file-encrypt/main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+	"bytes"
+
+	"golang.org/x/term"
+)
+
+func main(){
+	if len(os.Args) < 2{
+		printHelp()
+	}
+	function := os.Args[0]
+	switch function{
+	case "help":
+		printHelp();
+	case "encrypt":
+		encryptHandle()
+
+	case "decrypt":
+		decryptHandle()
+	default:
+		fmt.Println("Run encrypt to encrypt a file and decrypt to decrypt a file")
+		os.Exit(1)
+	}
+	
+
+}
+
+
+func printHelp() {
+    helpMessage := `
+Usage: [COMMAND] [OPTIONS]
+
+Commands:
+  encrypt   Encrypt a file or text.
+            Usage: encrypt [input] [output]
+            Example: encrypt myfile.txt encryptedfile.txt
+
+  decrypt   Decrypt a file or text.
+            Usage: decrypt [input] [output]
+            Example: decrypt encryptedfile.txt decryptedfile.txt
+
+  help      Display this help message.
+            Usage: help
+            Example: help
+
+Options:
+  -k, --key    Specify a key for encryption or decryption.
+               Example: go run . encrypt <fileName>
+
+Notes:
+  - Ensure you use the same key for encryption and decryption.
+  - Supported file formats: .txt, .json, etc.
+  - For any issues, contact support@example.com.
+`
+    fmt.Println(helpMessage)
+}
+
+
+func encryptHandle(){
+	if len(os.Args) < 3 {
+		println("missing the path to file. For more info run go run . help")
+	}
+	file := os.Args[2]
+
+	if !validateFile(file){
+		panic("File not found")
+	}
+
+}
+
+func decryptHandle(){
+
+}
+
+func getPassword()[]byte{
+
+	println("Enter the password")
+	password,_ := term.ReadPassword(0)
+	fmt.Println("Re Enter the password");
+	confirmPassword,_ := term.ReadPassword(0)
+
+	if !validatePassword(password,confirmPassword) {
+		fmt.Println("\n Passwords doesnt match")
+		return getPassword()
+	}
+
+	return password
+
+
+}
+
+//validate the password and its checks
+func validatePassword(password[] byte,confirmPassword []byte)bool{
+	if !bytes.Equal(password,confirmPassword){
+		return false
+	}
+	return true
+
+}
+
+//validate the file using inbuild os functions
+func validateFile(file string)bool{
+	if _,err := os.Stat(file);
+	os.IsNotExist(err){
+		return false
+	}
+
+	return true
+}
+
+
+
